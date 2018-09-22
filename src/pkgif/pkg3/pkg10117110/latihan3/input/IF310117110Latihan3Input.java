39c1250837b19ccdca96fe364c09d3aa37bbff8d
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-09-22 16:58:43 +0700

    first commit

---
+++ src/pkgif/pkg3/pkg10117110/latihan3/input/IF310117110Latihan3Input.java
@@ -0,0 +1,28 @@
+
+package pkgif.pkg3.pkg10117110.latihan3.input;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Nama Yang di input
+ */
+public class IF310117110Latihan3Input {
+
+
+    public static void main(String[] args) {
+        
+        System.out.print("Masukan Nama Anda: ");
+        Scanner scanner = new Scanner (System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah "+nama);
+       
+    }
+    
+}
